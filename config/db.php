1ff8990756e66a6e53f06d319be47808498a931a
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-10 22:22:04 +0530

    fgfh

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
 // last edited 10-11-2022 20:56
 if (($_SERVER["HTTP_HOST"] == "www.fastreed.com")||($_SERVER["HTTP_HOST"] == "fastreed.com")) {
-  $hostName = 'db5009716888.hosting-data.io';
+  $hostName = 'access918784663.hosting-data.io';
   $dbName = 'dbs8237639';
   $userName = 'dbu633513';
-  $passWord = 'TheLostIllusion@123';
+  $passWord = 'Rajedit@5119';
 }elseif ($_SERVER["HTTP_HOST"] == "localhost") {
   $hostName = 'localhost';
   $dbName = 'fastreed_db';
