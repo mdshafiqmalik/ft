54b4da7a0e0955643715e5b5aec1580d29ccc82a
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-10 22:13:27 +0530

    fghfg

--- admin/hard-refresh/index.php
+++ admin/hard-refresh/index.php
@@ -6,7 +6,7 @@
   </head>
   <body>
     <?php
-     $output = shell_exec("git pull fastreed main");
+     $output = shell_exec("git pull www main");
      echo "<pre>$output</pre>";
      ?>
   </body>
