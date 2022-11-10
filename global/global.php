0b8920f19bab94fb4354f6c297727f8cee9c4c06
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-10 22:15:43 +0530

    fdgfd

--- global/global.php
+++ global/global.php
@@ -53,13 +53,6 @@ function generateRandomID($keyLen){
    return $key;
 }
 
-
-function getIp(){
-  $ip = $_SERVER['REMOTE_ADDR'];
-  filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
-  return $ip;
-}
-
  ?>
 
 <?php
