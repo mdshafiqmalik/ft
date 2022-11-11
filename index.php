472c00d298c791b0bd3fcd331fc9ac5448652677
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-11 17:17:42 +0530

    dfgd

--- index.php
+++ index.php
@@ -2,3 +2,14 @@
 session_start();
 include 'cookies/visits.php';
 ?>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
