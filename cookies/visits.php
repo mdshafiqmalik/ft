6bb8becb6de1776162b4962014aa2a403d4bb208
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-11 16:59:20 +0530

    dfgdf

--- cookies/visits.php
+++ cookies/visits.php
@@ -151,7 +151,7 @@ function addNewVisitor(){
   // set session as visiterSId === cookie
   $_SESSION["visitorSID"] = $visitorID;
   // Set cookie
-  echo(addCookie($encryptedID));
+  var_dump(addCookie($encryptedID));
   // Add to visiter DB
   $sql = "INSERT INTO fast_visitor ( visitorId, visitorDevice, visitorBrowser, visitorPlatform, browserInfo ) VALUES ('$visitorID','$deviceType', '$browser', '$platform','$browserInfo')";
   mysqli_query($db, $sql);
