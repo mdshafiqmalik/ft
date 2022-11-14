7c5ac7ca91b38a6773d9a2fdeeff012b2002b15e
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-14 12:04:54 +0530

    fghfgh

--- global/global.php
+++ global/global.php
@@ -1,10 +1,4 @@
 <?php
-// last edited 10-11-2022 20:56
-// generateUniqueID function create a random number i.e. 93479139632491634
-// Takes two arguments fields and keyLength
-// $keyLength: Lenght of ID i.e. 20
-// $fields: It is an array contains ["table"] and ["row"] to check it is unique in a db
-// need just random ID then call generateRandomID($keyLength);
 function generateUniqueID($fields, $keyLength){
   $randomString = "";
   for ($x = 1; $x <= $keyLength; $x++) {
@@ -18,11 +12,8 @@ function generateUniqueID($fields, $keyLength){
     }
     $checkInDB = checkInDB($fields,$randomString);
   }
-
   return $randomString;
 }
-
-// Checking in Database
 function checkInDB($fields, $randomString){
 $dbc = $_SERVER['DOCUMENT_ROOT'].'/config/db.php';
 include($dbc);
@@ -39,26 +30,18 @@ while ($row = mysqli_fetch_assoc($result)) {
 }
 return $idExists;
 }
- ?>
-
-<?php
 function createOTP($keyLen){
-  // Set a blank variable to store the key in
    $key = "";
    for ($x = 1; $x <= $keyLen; $x++) {
-       // Set each digit
        $key .= random_int(0, 9);
    }
    return $key;
 }
-
-
 function getIp(){
   $ip = $_SERVER['REMOTE_ADDR'];
   filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
   return $ip;
 }
-
 function getDeviceInfo(){
   if (isset($_SERVER['HTTP_USER_AGENT'])) {
     $agent = $_SERVER['HTTP_USER_AGENT'];
