0f5889519211fbe15d1364512039a8d6a5eaa755
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-14 17:15:13 +0530

    rtytr

--- test.php
+++ test.php
@@ -5,3 +5,13 @@
   $sql2 = "INSERT INTO fast_sessions (sessionID, visitorIP, visitorID, sessionVisits) VALUES ('8687890978654532456754','199.89.45.234','3454352765756476574','{}')";
   mysqli_query($db, $sql2);
  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <a href="/"> dsgfdgf</a>
+  </body>
+</html>
