69ff9232978557782ebb2a4058901e92141f81e3
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-14 12:06:36 +0530

    hjkjh

--- global/global.php
+++ global/global.php
@@ -1,4 +1,10 @@
 <?php
+// last edited 10-11-2022 20:56
+// generateUniqueID function create a random number i.e. 93479139632491634
+// Takes two arguments fields and keyLength
+// $keyLength: Lenght of ID i.e. 20
+// $fields: It is an array contains ["table"] and ["row"] to check it is unique in a db
+// need just random ID then call generateRandomID($keyLength);
 function generateUniqueID($fields, $keyLength){
   $randomString = "";
   for ($x = 1; $x <= $keyLength; $x++) {
@@ -12,8 +18,11 @@ function generateUniqueID($fields, $keyLength){
     }
     $checkInDB = checkInDB($fields,$randomString);
   }
+
   return $randomString;
 }
+
+// Checking in Database
 function checkInDB($fields, $randomString){
 $dbc = $_SERVER['DOCUMENT_ROOT'].'/config/db.php';
 include($dbc);
@@ -30,18 +39,24 @@ while ($row = mysqli_fetch_assoc($result)) {
 }
 return $idExists;
 }
+
 function createOTP($keyLen){
+  // Set a blank variable to store the key in
    $key = "";
    for ($x = 1; $x <= $keyLen; $x++) {
+       // Set each digit
        $key .= random_int(0, 9);
    }
    return $key;
 }
+
+
 function getIp(){
   $ip = $_SERVER['REMOTE_ADDR'];
   filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
   return $ip;
 }
+
 function getDeviceInfo(){
   if (isset($_SERVER['HTTP_USER_AGENT'])) {
     $agent = $_SERVER['HTTP_USER_AGENT'];
--- test.php
+++ test.php
@@ -1,39 +1,4 @@
 <?php
-// last edited 10-11-2022 20:56
-function generateUniqueID(){
-  $randomString = "";
-  for ($x = 1; $x <= 20; $x++) {
-      $randomString .= random_int(0, 9);
-  }
-  $checkInDB = checkInDB();
-  while ($checkInDB == true) {
-    for ($x = 1; $x <= $keyLength; $x++) {
-        $randomString = "";
-        $randomString .= random_int(0, 9);
-    }
-    $checkInDB = checkInDB();
-  }
-
-  return $randomString;
-}
-
-function checkInDB(){
-$dbc = $_SERVER['DOCUMENT_ROOT'].'/config/db.php';
-include($dbc);
-$sql = "SELECT * FROM fast_visitor";
-$result = mysqli_query($db, $sql);
-$idExists = false;
-while ($row = mysqli_fetch_assoc($result)) {
-  if ($row["visitorID"] == "3543645643634") {
-    $idExists = true;
-    break;
-  }else {
-    $idExists = false;
-  }
-}
-return $idExists;
-}
-checkInDB();
 $cookieSet = setcookie('visitorID', "ghjh", time() + (86400 * 30), "/");
 var_dump($cookieSet);
 
