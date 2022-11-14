fcb5e1d9e407a12a4d4ce9e9f303f7eaaa417505
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-14 17:02:37 +0530

    ghjhg

--- cookies/visits.php
+++ cookies/visits.php
@@ -129,8 +129,8 @@ function updateSessionActivity($pagesViews, $sessionID){
   $array = array("$dateTime" => "$httpReferer");
   $hRef = json_encode($array);
 
-  if(isset($_GET["name"]) && !empty($_GET["name"])){
-    $visitReferer = $_GET["name"];
+  if(isset($_GET["referer"]) && !empty($_GET["name"])){
+    $visitReferer = $_GET["referer"];
   }else {
     $visitReferer = "No Referer";
   }
