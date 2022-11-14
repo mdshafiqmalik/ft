8761dc86fc202876c390b0c1b0489debfd47dcb5
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-14 12:01:18 +0530

    fgh

--- test.php
+++ test.php
@@ -1,5 +1,21 @@
 <?php
 // last edited 10-11-2022 20:56
+function generateUniqueID(){
+  $randomString = "";
+  for ($x = 1; $x <= 20; $x++) {
+      $randomString .= random_int(0, 9);
+  }
+  $checkInDB = checkInDB();
+  while ($checkInDB == true) {
+    for ($x = 1; $x <= $keyLength; $x++) {
+        $randomString = "";
+        $randomString .= random_int(0, 9);
+    }
+    $checkInDB = checkInDB();
+  }
+
+  return $randomString;
+}
 
 function checkInDB(){
 $dbc = $_SERVER['DOCUMENT_ROOT'].'/config/db.php';
