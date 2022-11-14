007f6e9c1198f1e2be591d8181800ef32588d04c
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-14 11:56:41 +0530

    fgfh

--- test.php
+++ test.php
@@ -1,5 +1,24 @@
 <?php
 // last edited 10-11-2022 20:56
+
+function checkInDB("3543645643634"){
+$dbc = $_SERVER['DOCUMENT_ROOT'].'/config/db.php';
+include($dbc);
+$sql = "SELECT * FROM fast_visitor";
+$result = mysqli_query($db, $sql);
+$idExists = false;
+while ($row = mysqli_fetch_assoc($result)) {
+  if ($row["visitorID"] == $randomString) {
+    $idExists = true;
+    break;
+  }else {
+    $idExists = false;
+  }
+}
+return $idExists;
+}
+
 $cookieSet = setcookie('visitorID', "ghjh", time() + (86400 * 30), "/");
 var_dump($cookieSet);
+
  ?>
