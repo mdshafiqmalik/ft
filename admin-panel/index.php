6269bb11fd958fb62a68fa0a55bf64c1e0ee8a09
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-19 22:57:44 +0530

    fggf

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -10,7 +10,7 @@
     <div class="top-div">
       <!--  Navbar -->
       <nav>
-        <div class="nav-G1">
+        <div id="nav-G1" class="nav-G1">
           <div class="nav-logo nav-contents">
             <a id="logo" href="/">FastReed <span>admin</span> </a>
             <div id="small-logo"> <a href="/admin-panel"><img height="50px" width="55px" src="assets/images/logo-mini.svg" alt=""></a> </div>
@@ -20,13 +20,16 @@
             <img src="assets/images/icons/menu.svg" alt="">
           </div>
         </div>
-        <div class="nav-G2">
+        <div id="nav-G1" class="nav-G2">
           <div id="fullscreen">
             <img id="fullscreen-icon" src="assets/images/icons/fullscreen.svg" alt="">
           </div>
 
           <div id="nav-profile">
             <img id="nav-profile-img" src="assets/images/shafiq.jpeg" alt="">
+          </div>
+          <div id="nav-right-menu">
+            <img src="assets/images/icons/menu.svg" alt="">
           </div>
         </div>
       </nav>
