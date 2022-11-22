791eda6f5b6e6ea551772951fb866997e7f7e9f5
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-23 01:12:39 +0530

    hjgjhgjhg

--- admin-panel/assets/js/all.js
+++ admin-panel/assets/js/all.js
@@ -2,20 +2,20 @@ document.getElementById('fullscreen').onclick = function(){
   if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
     if (document.documentElement.requestFullScreen) {
       document.documentElement.requestFullScreen();
-      document.getElementById('fullscreen-icon').src = "assets/images/icons/minimize.svg";
+      document.getElementById('fullscreen-icon').src = "../assets/svgs/minimize.svg";
     } else if (document.documentElement.mozRequestFullScreen) {
       document.documentElement.mozRequestFullScreen();
-      document.getElementById('fullscreen-icon').src = "assets/images/icons/minimize.svg";
+      document.getElementById('fullscreen-icon').src = "../assets/svgs/minimize.svg";
     } else if (document.documentElement.webkitRequestFullScreen) {
       document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-      document.getElementById('fullscreen-icon').src = "assets/images/icons/minimize.svg";
+      document.getElementById('fullscreen-icon').src = "../assets/svgs/minimize.svg";
     } else if (document.documentElement.msRequestFullscreen) {
       document.documentElement.msRequestFullscreen();
-      document.getElementById('fullscreen-icon').src = "assets/images/icons/minimize.svg";
+      document.getElementById('fullscreen-icon').src = "../assets/svgs/minimize.svg";
     }
 
   } else {
-    document.getElementById('fullscreen-icon').src = "assets/images/icons/fullscreen.svg";
+    document.getElementById('fullscreen-icon').src = "../assets/svgs/fullscreen.svg";
     if (document.cancelFullScreen) {
       document.cancelFullScreen();
     } else if (document.mozCancelFullScreen) {
@@ -25,5 +25,21 @@ document.getElementById('fullscreen').onclick = function(){
     } else if (document.msExitFullscreen) {
       document.msExitFullscreen();
     }
+  }
+}
+let logo = document.getElementById('logo');
+let smallLogo = document.getElementById('small-logo');
+let leftMenu = document.getElementById('nav-left-menu');
+let profile = document.getElementById('sideMenu-profile');
+
+function toggleNav(){
+  if (logo.style.display === "none") {
+    logo.style.display = "block";
+    smallLogo.style.display = "none";
+    profile.style.display = "flex";
+  } else {
+    logo.style.display = "none";
+    smallLogo.style.display = "block";
+    profile.style.display = "none";
   }
 }
--- admin-panel/index.php
+++ admin-panel/index.php
@@ -13,26 +13,76 @@
         <div id="nav-G1" class="nav-G1">
           <div class="nav-logo nav-contents">
             <a id="logo" href="/">FastReed <span>admin</span> </a>
-            <div id="small-logo"> <a href="/admin-panel"><img height="50px" width="55px" src="assets/images/logo-mini.svg" alt=""></a> </div>
+            <div id="small-logo"> <a href="/admin-panel"><img width="40px" height="48px" src="../assets/images/favicon.png" alt=""></a> </div>
           </div>
 
-          <div id="nav-left-menu">
-            <img src="assets/images/icons/menu.svg" alt="">
+          <div onclick="toggleNav()" id="nav-left-menu">
+            <img src="../assets/svgs/menu_open.svg" alt="">
           </div>
         </div>
         <div id="nav-G1" class="nav-G2">
           <div id="fullscreen">
-            <img id="fullscreen-icon" src="assets/images/icons/fullscreen.svg" alt="">
+            <img id="fullscreen-icon" src="../assets/svgs/fullscreen.svg" alt="">
           </div>
 
           <div id="nav-profile">
-            <img id="nav-profile-img" src="assets/images/shafiq.jpeg" alt="">
+            <img id="nav-profile-img" src="../assets/images/face1.jpg" alt="">
           </div>
           <div id="nav-right-menu">
-            <img src="assets/images/icons/menu.svg" alt="">
+            <img src="../assets/svgs/menu.svg" alt="">
           </div>
         </div>
       </nav>
+
+      <div id="sideMenu" class="sideMenu">
+        <ul class="sideMenusList" id="sideMenus">
+          <li class="sideMenusList" id="sideMenu-profile" >
+            <a href="#">
+              <div class="image">
+                <img src="../assets/images/face1.jpg" alt="">
+              </div>
+              <div class="about">
+                <span class="name">David Grey H.</span>
+                <span class="desig">Project Admin</span>
+              </div>
+              <div class="greenTickBadge">
+                <img src="../assets/svgs/verified.svg" alt="">
+              </div>
+            </a>
+          </li>
+          <li class="sideMenusList">
+            <a class="menus" href="#">Home</a>
+            <div class="menusIcon">
+              <img   height="22px" width="22px" src="../assets/svgs/home.svg" alt="">
+            </div>
+          </li>
+          <li class="sideMenusList">
+            <a class="menus"  href="#">Stats</a>
+            <div class="menusIcon">
+              <img  height="22px" width="22px" src="../assets/svgs/stats.svg" alt="">
+            </div>
+          </li>
+          <li class="sideMenusList">
+            <a class="menus"  href="#">Users</a>
+            <div class="menusIcon">
+              <img   height="22px" width="22px" src="../assets/svgs/user.svg" alt="">
+            </div>
+          </li>
+          <li class="sideMenusList">
+            <a class="menus"  href="#">Settings</a>
+            <div class="menusIcon">
+              <img   height="22px" width="22px" src="../assets/svgs/settings.svg" alt="">
+            </div>
+          </li>
+          <li class="sideMenusList">
+            <a class="menus"  href="#">Logout</a>
+            <div class="menusIcon">
+              <img   height="22px" width="22px" src="../assets/svgs/power.svg" alt="">
+            </div>
+          </li>
+        </ul>
+      </div>
+
     </div>
   </body>
   <script src="assets/js/all.js?v=<?php echo getenv('cssVersion'); ?>" charset="utf-8"></script>
