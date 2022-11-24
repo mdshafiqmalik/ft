6c56298a2b7ebbeb39f3cd268636990a0bd5674e
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-24 13:38:14 +0530

    gjghj

--- admin/index.php
+++ admin/index.php
@@ -1,78 +1,144 @@
-<?php
-// last edited 10-11-2022 20:56
-session_start();
-$visits = $_SERVER['DOCUMENT_ROOT'].'/cookies/visits.php';
-include($visits);
-if (isset($_SESSION['adminLoginID'])) {
-  if ($adminId = checkLogDetails($_SESSION['adminLoginID'])) {
-    $adminDetails = getAdminDetails($adminId);
-  }else {
-    header("Location: login/");
-  }
-}else {
-  header("Location: login/");
-}
- ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="icon" type="image/x-icon" href="/favicon.svg">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="robots" content="noindex">
-    <title>Fastreed - Admin Console</title>
-    <style media="screen">
-    *{
-      margin: 0;
-      padding: 0;
-    }
-      body{
-        display: flex;
-        justify-content: center;
-      }
-      .mainCont{
-        max-width: 500px;
-        width: 100%;
-        background-color: #00ced1;
-        height: 100%;
-        min-height: 100vh;
-      }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/css/style.css?v=<?php echo getenv('cssVersion'); ?>">
+    <title>Fastreed: Admin Panel</title>
   </head>
   <body>
-    <div class="mainCont">
-      <a href="logout"> Logout</a>
+    <div class="top-div">
+      <!--  Navbar -->
+      <nav>
+        <div id="nav-G1" class="nav-G1">
+          <div class="nav-logo">
+            <a id="logo" href="/">FastReed <span>.com/admin</span> </a>
+            <div id="small-logo"> <a href="/admin-panel"><img width="40px" height="48px" src="../assets/images/favicon.png" alt=""></a> </div>
+          </div>
+
+          <div onclick="toggleNav()" id="nav-left-menu">
+            <img src="../assets/svgs/menu_lines.svg" alt="">
+          </div>
+        </div>
+
+        <div id="nav-G2" class="nav-G2">
+          <div id="nav-profile">
+            <img id="nav-profile-img" src="../assets/images/face1.jpg" alt="">
+          </div>
+          <div id="fullscreen">
+            <img id="fullscreen-icon" src="../assets/svgs/fullscreen.svg" alt="">
+          </div>
+
+
+          <!-- <div id="nav-right-menu">
+            <img src="../assets/svgs/menu_lines.svg" alt="">
+          </div> -->
+        </div>
+      </nav>
+      <div class="mainBody">
+
+        <div id="sideMenu" class="sideMenu">
+          <div id="navInfo" hidden>notCollapsed</div>
+          <ul class="sideMenusList" id="sideMenus">
+
+            <li class="sideMenusList" id="sideMenu-profile" >
+              <a id="sideProfile" href="#">
+                <div class="image">
+                  <img src="../assets/images/face1.jpg" alt="">
+                </div>
+                <div class="about">
+                  <span class="name">David Grey H.</span>
+                  <span class="desig">Project Admin</span>
+                </div>
+                <div class="greenTickBadge">
+                  <img src="../assets/svgs/verified.svg" alt="">
+                </div>
+              </a>
+            </li>
+
+            <li class="sideMenusList">
+              <div class="mainMenu">
+                  <a class="menus" href="#">Home</a>
+                  <div class="menusIcon">
+                    <img height="22px" width="22px" src="../assets/svgs/home.svg" alt="">
+                  </div>
+              </div>
+              <div class="subMenu"></div>
+            </li>
+
+            <li class="sideMenusList">
+              <div class="mainMenu">
+                  <a class="menus"  href="#">Stats</a>
+                  <div class="menusIcon">
+                    <img height="22px" width="22px" src="../assets/svgs/stats.svg" alt="">
+                  </div>
+              </div>
+              <div class="subMenu">  </div>
+            </li>
+
+            <li class="sideMenusList">
+              <div class="mainMenu">
+                  <a class="menus"  href="#">Users</a>
+                  <div class="menusIcon">
+                    <img height="22px" width="22px" src="../assets/svgs/user.svg" alt="">
+                  </div>
+              </div>
+              <div class="subMenu">  </div>
+            </li>
+
+            <li class="sideMenusList">
+              <div class="mainMenu">
+                  <a class="menus"  href="#">Settings</a>
+                  <div class="menusIcon">
+                    <img height="22px" width="22px" src="../assets/svgs/settings.svg" alt="">
+                  </div>
+              </div>
+              <div class="subMenu">  </div>
+            </li>
+
+            <li class="sideMenusList dropdownMenu">
+              <div class="mainMenu" onclick="expandMenus(6)">
+                <span hidden> 0 </span>
+                <a class="menus"  href="#">Category</a>
+                <img  class="dropdownIcon-b" height="10px" width="10px" src="../assets/svgs/dropdown.svg" alt="">
+                <div class="menusIcon">
+                  <img class="dropdownIcon-a" height="10px" width="10px" src="../assets/svgs/dropdown.svg" alt="">
+                  <img height="22px" width="22px" src="../assets/svgs/channel.svg" alt="">
+
+                </div>
+              </div>
+              <div class="subMenu">
+                <ul display="none"class="subMenu-ul">
+                  <li> <a href="#">Cricket</a> </li>
+                  <li><a href="#">Vollyball</a></li>
+                </ul>
+              </div>
+            </li>
+
+            <li class="sideMenusList">
+              <div class="mainMenu">
+                  <a class="menus"  href="#">Logout</a>
+                  <div class="menusIcon">
+                    <img height="22px" width="22px" src="../assets/svgs/power.svg" alt="">
+                  </div>
+              </div>
+              <div class="subMenu">  </div>
+            </li>
+
+
+          </ul>
+        </div>
+
+        <div class="pageBody">
+          <div class="div1">
+
+          </div>
+          <div class="div2">
+
+          </div>
+        </div>
+      </div>
     </div>
   </body>
+  <script src="assets/js/all.js?v=<?php echo getenv('cssVersion'); ?>" charset="utf-8"></script>
 </html>
-
-
-
-<?php
-function checkLogDetails($logID){
-  include $_SERVER['DOCUMENT_ROOT'].'/.htpasswd/db.php';
-  $checkLoginID = "SELECT adminID FROM admin_log_history Where loginID = '$logID' AND status = '1'";
-  $userDat = mysqli_query($db, $checkLoginID);
-  if (mysqli_num_rows($userDat)) {
-      $row = $userDat->fetch_assoc();
-      $exist = $row['adminID'];
-  }else {
-    unset($_SESSION['adminLoginID']);
-    $exist = false;
-  }
-  return $exist;
-}
-
-function getAdminDetails($adminId){
-  include $_SERVER['DOCUMENT_ROOT'].'/.htpasswd/db.php';
-  $sql = "SELECT * FROM fast_admin WHERE adminID = '$adminId'";
-  $result = mysqli_query($db, $sql);
-  if (mysqli_num_rows($result)) {
-    $row = $result->fetch_assoc();
-    $data = $row;
-  }else {
-    $data = "no user found";
-  }
-  return $data;
-}
- ?>
