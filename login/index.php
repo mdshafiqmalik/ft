9a8f6567edbdb947c4cb927aa2ec37612c86240e
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-24 18:19:27 +0530

    gfjhgj

--- login/index.php
+++ login/index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="/assets/css/login.css?v=<?php echo getenv('cssVersion'); ?>">
-    <title>Fastreed: Admin Login</title>
+    <link rel="stylesheet" href="assets/style.css?v=<?php echo getenv('cssVersion'); ?>">
+    <title>Fastreed: User Login</title>
   </head>
   <body>
     <div class="loginOuterDiv">
       <div class="loginElements brandLogo">
-        <a href="#">FastReed <span>admin</span> </a>
+        <a href="#">FastReed <span>login</span> </a>
       </div>
       <div class="loginElements headingsAndErrors">
         <span class="greetHeading">Hello! let's get started</span>
@@ -18,7 +20,7 @@
       <form class="loginElements loginForm" action="" method="post">
         <input class="fields" type="text" name="username" value="" placeholder="Username">
         <input class="fields" type="current-password" name="current-password" value="" placeholder="Password">
-        <input id="submit" class="submit" type="submit" name="Submit" value="Login">
+        <input id="submit" type="submit" name="Submit" value="Login">
 
       </form>
       <div class="loginElements forgotPassword">
--- register/index.php
+++ register/index.php
@@ -1,32 +1,31 @@
 <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="/assets/css/login.css?v=<?php echo getenv('cssVersion'); ?>">
-    <title>Fastreed: Admin Login</title>
+    <link rel="stylesheet" href="assets/style.css?v=<?php echo getenv('cssVersion'); ?>">
+    <title>Fastreed: User Regsitration</title>
   </head>
   <body>
     <div class="loginOuterDiv">
       <div class="loginElements brandLogo">
-        <a href="#">FastReed <span>admin</span> </a>
+        <a href="#">FastReed <span>register</span> </a>
       </div>
       <div class="loginElements headingsAndErrors">
-        <span class="greetHeading">Hello! let's get started</span>
-        <span class="messageAndErrors">Sign In to continue</span>
+        <span class="greetHeading">Create a new account</span>
+        <span class="messageAndErrors">All fields required</span>
       </div>
       <form class="loginElements loginForm" action="" method="post">
         <input class="fields" type="text" name="username" value="" placeholder="Username">
+        <input class="fields" type="email" name="username" value="" placeholder="Email">
         <input class="fields" type="current-password" name="current-password" value="" placeholder="Password">
-        <input id="submit" class="submit" type="submit" name="Submit" value="Login">
+        <input id="submit" class="submit" type="submit" name="Submit" value="Register">
 
       </form>
-      <div class="loginElements forgotPassword">
-        <input type="checkbox" name="" value="">
-        <p>Remember this device</p>
-      </div>
-      <div class="">
-
+      <div class="others">
+        <a href="#">Login Here</a>
       </div>
     </div>
   </body>
