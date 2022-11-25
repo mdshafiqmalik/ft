d04c1fbd28ca2d7978593389ab7c492fc4e44b8e
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-11-25 22:31:22 +0530

    fghgfdh

--- admin/login/auth.php
+++ admin/login/auth.php
@@ -1,20 +1,20 @@
 
 <?php
-    // $email; $comment; $captcha;
-    //
-    // if(isset($_POST['username']))
-    //     $username=$_POST['username'];
-    // if(isset($_POST['password']))
-    //     $password=$_POST['password'];
-    // if(isset($_POST['g-recaptcha-response']))
-    //     $captcha=$_POST['g-recaptcha-response'];
-    //
-    // if(!$captcha){
-    //     echo '<h2>Please check the the captcha form.</h2>';
-    //     exit;
-    // }
+    $email; $comment; $captcha;
 
-    $response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LcFdOMbAAAAABVlj4_7eGdQ2Ha_3vHayE2YMoGP&response="."dsuhsdfiougdfsuogdf"."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
+    if(isset($_POST['username']))
+        $username=$_POST['username'];
+    if(isset($_POST['password']))
+        $password=$_POST['password'];
+    if(isset($_POST['g-recaptcha-response']))
+        $captcha=$_POST['g-recaptcha-response'];
+
+    if(!$captcha){
+        echo '<h2>Please check the the captcha form.</h2>';
+        exit;
+    }
+
+    $response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LcFdOMbAAAAABVlj4_7eGdQ2Ha_3vHayE2YMoGP&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
     var_dump($response['success']);
 
 ?>
