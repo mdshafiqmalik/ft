57edee576c4d99d2ef690c6b87ac204b1b9f90c1
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-12-01 18:52:05 +0530

    gfjhg

--- admin/login/auth.php
+++ admin/login/auth.php
@@ -31,18 +31,18 @@ function userName(){
       }else {
         $userNameRes['valid'] = false;
         $_SESSION['authStatus'] = "Incorrect Username";
-        header("Location: /admin/login/index.php?s=1");
+        header("Location: /admin/login/index.php?err=AE01");
       }
     }else {
       $userNameRes['valid'] = false;
       $_SESSION['authStatus'] = "Username Cannot Be Empty";
-      header("Location: /admin/login/index.php?s=2");
+      header("Location: /admin/login/index.php?err=AE02");
       exit;
     }
   }else {
     $userNameRes['valid'] = false;
     $_SESSION['authStatus'] = "Username Not Found In Form";
-    header("Location: /admin/login/index.php?s=3");
+    header("Location: /admin/login/index.php?err=AE03");
     exit;
   }
   return $userNameRes;
@@ -75,19 +75,19 @@ function passWord($adID){
       }else {
         $_SESSION['authStatus'] = "Incorrect Password";
         $passWordRes['valid'] = false;
-        header("Location: /admin/login?s=4");
+        header("Location: /admin/login?err=AE04");
         exit;
       }
     }else {
       $passWordRes['valid'] = false;
       $_SESSION['authStatus']= "Password Is Empty";
-      header("Location: /admin/login?s=5");
+      header("Location: /admin/login?err=AE05");
       exit;
     }
   }else {
     $passWordRes['valid'] = false;
     $_SESSION['authStatus']= "Password Not Included In Form";
-    header("Location: /admin/login?s=6");
+    header("Location: /admin/login?err=AE06");
     exit;
   }
   return $passWordRes;
@@ -126,19 +126,19 @@ function captchaResponse(){
         // G_recaptcha not Authorized
         $_SESSION['authStatus'] ="Captcha Not Valid";
         $captchaRes['valid'] = false;
-        header("Location: /admin/login?s=7");
+        header("Location: /admin/login?err=AE07");
         exit;
       }
     }else {
       $_SESSION['authStatus'] ="Refill The Captcha";
       $captchaRes['valid'] = false;
-      header("Location: /admin/login?s=8");
+      header("Location: /admin/login?err=AE08");
       exit;
     }
   }else {
     $_SESSION['authStatus'] ="Captcha Not Included In Form";
     $captchaRes['valid'] = false;
-    header("Location: /admin/login?s=9");
+    header("Location: /admin/login?err=AE09");
     exit;
   }
   return $captchaRes;
@@ -193,25 +193,25 @@ function deviceStatus($userID){
         }else {
           $_SESSION['authStatus'] = "Device Logged Out";
           $deviceLogged = false;
-          header("Location: /admin/login?s=10");
+          header("Location: /admin/login?err=AE10");
           exit;
         }
       }else {
         $_SESSION['authStatus'] = "Invalid Device ID";
         $deviceLogged = false;
-        header("Location: /admin/login?s=11");
+        header("Location: /admin/login?err=AE11");
         exit;
       }
     }else {
       $_SESSION['authStatus'] ="Device not added as admin";
       $deviceLogged = false;
-      header("Location: /admin/login?s=12");
+      header("Location: /admin/login?err=AE12");
       exit;
     }
   }else {
     $_SESSION['authStatus'] = "Device not added as admin";
     $deviceLogged = false;
-    header("Location: /admin/login?s=13");
+    header("Location: /admin/login?err=AE13");
     exit;
   }
   return $deviceLogged;
