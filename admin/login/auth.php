cabf7627e3a4e57c8d5a45e47106a3b2731d0e4f
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-12-01 15:35:37 +0530

    jhgj

--- admin/login/auth.php
+++ admin/login/auth.php
@@ -16,6 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       if (passWord($adminID)['valid']) {
         if (deviceStatus($adminID)) {
           include $domain. '/.htHidden/functions/activityConverter.php';
+          echo "Logged";
         }
       }
     }
