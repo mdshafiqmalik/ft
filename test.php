68a133e693edbd239220a75817b9208d143e09b5
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-12-02 13:46:54 +0530

    fghgh

--- test.php
+++ test.php
@@ -1,34 +1,4 @@
 <?php
-// date_default_timezone_set("asia/kolkata");
-// $hrs = (int) date('h');
-// $min = (int) date('i');
-// $day = (int) date('d');
-// $key = (int) $hrs*$min*$day;
-// echo $key;
-// // $ulen = (boolean) preg_match('/\s/', 'dfgfd dkg');
-// // var_dump($ulen);
-// function getCaptchaKey(){
-//   include '.htHidden/s_keys/db.php';
-//   $sql = "SELECT credKey FROM secretCredentials WHERE credKey = 'G_RECAPTCHA'";
-//   $result = mysqli_query($db, $sql);
-// }
 include '../secrets/encDec.php';
 include '../secrets/db.php';
-// include '../secrets/encDec.php';
-
-
-
-// i3T7pcU2Ip90pQYYpBsFkJgl ==  DID202212020000000
-// i3T7pcU2Ip93pwwYpBsFkJgk == DID202211280000001
-
-
-$sql = "SELECT deviceID FROM deviceManager WHERE deviceID = 'DI2022_-1128g0001'";
-$result = mysqli_query($db, $sql);
-if ($result) {
-  echo "i3T7pcU2Ip90pQYYpBsFkJgl";
-  echo "<br><br>";
-  echo "i3T7pcU2Ip93pwwYpBsFkJgk";
-}else {
-echo "false";
-}
  ?>
