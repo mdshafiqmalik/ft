972389c4d6b89a162b91da03bf56cd5746d297a7
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-12-08 23:38:18 +0530

    gfhg

--- test.php
+++ test.php
@@ -1,16 +1,6 @@
 <?php
 include '../secrets/encDec.php';
 include '../secrets/db.php';
-/**
- * 
- */
-class ClassName extends AnotherClass
-{
-
-  function __construct(argument)
-  {
-    // code...
-  }
-}
+var_dump($_SERVER)
 
  ?>
