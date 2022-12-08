2341612f277b9a2351b0f65891d1e282e3198922
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-12-08 22:38:21 +0530

    fhgf

--- test.php
+++ test.php
@@ -1,16 +1,16 @@
 <?php
 include '../secrets/encDec.php';
 include '../secrets/db.php';
+/**
+ * 
+ */
+class ClassName extends AnotherClass
+{
 
-// $sql2 = "SELECT loggedStatus FROM deviceManager WHERE deviceID = 'DID202212020000000'";
-// $result2 = mysqli_query($db, $sql2);
-// $row = $result2->fetch_assoc();
+  function __construct(argument)
+  {
+    // code...
+  }
+}
 
-$sql = "SELECT deviceID,linkStatus FROM deviceManager WHERE deviceID = 'DID202212020000000'";
-$result = mysqli_query($db, $sql);
-$row = $result->fetch_assoc();
-echo $row['deviceID'];
-var_dump('DID202212020000000' === $row['deviceID'] );
-echo "<br>";
-echo $row['linkStatus'];
  ?>
