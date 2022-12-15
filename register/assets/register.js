e04abc8dd4db9c4e02915530e68a7ff9d987d26d
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-12-16 01:18:06 +0530

    fdgfdgd

--- register/assets/register.js
+++ register/assets/register.js
@@ -135,7 +135,7 @@ function checkInviteID(){
     const registerAPI =`/.htHidden/API/Internal/register.php?inviteID=${userInput}`;
     checkInviteID(registerAPI);
     async function checkInviteID(url){
-      userError.innerHTML = " Validating....";
+      userError.innerHTML = " Checking....";
       userInputField.style.boxShadow = "0px 0px 3px 0px #1dff00";
       userError.style.color = "green";
       const response = await fetch(url);
@@ -188,7 +188,7 @@ function checkEmail(){
   }else {
     checkEmailExists(registerAPI);
     async function checkEmailExists(url){
-      userError.innerHTML = " Validating....";
+      userError.innerHTML = " Checking....";
       userInputField.style.boxShadow = "0px 0px 3px 0px #1dff00";
       userError.style.color = "green";
       const response = await fetch(url);
