e639092b035a74f4160c70c08a32873865d4ea34
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-12-21 15:47:57 +0530

    gfhfg

--- .htHidden/activity/VISIT.php
+++ .htHidden/activity/VISIT.php
@@ -5,7 +5,7 @@ if (!isset($_SERVROOT)) {
 }
 
 
-$GLOBALS['PATH_HELPER'] = '/kunden/homepages/40/d918784663/PATH_HELPER.php';
+$GLOBALS['PATH_HELPER'] = '/kunden/homepages/40/d918784663/htdocsPATH_HELPER.php';
 
 include_once($GLOBALS['PATH_HELPER']);
 $_DOCROOT = $_SERVER['DOCUMENT_ROOT'];
